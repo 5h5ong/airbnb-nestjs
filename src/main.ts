c59a9b5ae2fd790e5511354897b2e32199e9c82c
call5h5ong <call5h5ong@gmail.com>
2021-04-17 23:57:26 +0900

    feat : 들어온 데이터에 유효성 검사 사용
    DTO와 맞지 않는 property가 있을 때 경고를 띄워주고 Request를 받지 않음. property가 DTO에 명시된 타입으로 변환 가능하게 함(ex. string -> number).

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Request 유효성 검사
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // DTO에 없는 property는 받지 않음
+      whitelist: true,
+      // DTO에 없는 property가 있을 때 Request 취소 및 경고 표시
+      forbidNonWhitelisted: true,
+      // 들어온 데이터의 타입을 알맞게 변환
+      transform: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
