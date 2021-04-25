fbca6b74f9b59290ccaded084ee4bb133eeda89c
call5h5ong <call5h5ong@gmail.com>
2021-04-25 19:08:25 +0900

    feat : dotfile 사용 설정
    "@nestjs/config"를 이용해 dotfile(.env)를 사용할 수 있게 만듬.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccommodationsModule } from './accommodations/accommodations.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AccommodationsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.dev',
+    }),
+    AccommodationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
