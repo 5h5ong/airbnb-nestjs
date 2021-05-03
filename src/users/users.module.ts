99c6ee53f69b7f6f9718a1f1d4f2316676e987fa
call5h5ong <call5h5ong@gmail.com>
2021-05-03 23:21:28 +0900

    refactor : "authProvider"로 대체
    원래 "user.service"안에 bcrypt를 사용한 function이 구현되어 있었는데 그걸 때어내서 "authProvider"로 옮김. 이제는 "authProvider"를 통해 function을 사용함.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UsersDatabaseModule } from 'src/database/users-database/users-database.module';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthLibModule } from 'src/auth/libs/auth-lib.module';
 
 @Module({
   imports: [
     UsersDatabaseModule,
+    AuthLibModule,
     JwtModule.registerAsync({
       useFactory: () => ({
         secret: process.env.JWT_SECRET,
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,42 +1,24 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { createUsersDto } from 'src/users/dto/create-users.dto';
 import { UsersDatabaseService } from 'src/database/users-database/users-database.service';
-import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { authUserDto } from './dto/auth-users.dto';
+import { AuthLibs } from 'src/auth/libs/auth.lib';
 
 @Injectable()
 export class UsersService {
   constructor(
     private readonly usersDatabaseService: UsersDatabaseService,
     private readonly jwtService: JwtService,
+    @Inject('AUTH')
+    private readonly authProvider: AuthLibs,
   ) {}
-
-  /**
-   * Create Hash using the password.
-   */
-  createPasswordHash(password: string) {
-    const saltOrRounds = 10;
-    const hashedPassword = bcrypt.hashSync(password, saltOrRounds);
-    return hashedPassword;
-  }
-
-  /**
-   * 입력된 패스워드와 해시가 일치하는지 확인함
-   *
-   * @param plainPassword 암호화되지 않은 패스워드
-   * @param hash          해시로 변환되어 있는 패스워드
-   */
-  comparePassword(plainPassword: string, hash: string) {
-    const result = bcrypt.compareSync(plainPassword, hash);
-    return result;
-  }
 
   create(usersData: createUsersDto) {
     const { password } = usersData;
 
     // Hashing Password
-    const hashedPassword = this.createPasswordHash(password);
+    const hashedPassword = this.authProvider.createPasswordHash(password);
 
     // Create User Document
     return this.usersDatabaseService.create({
@@ -61,7 +43,7 @@ export class UsersService {
     }
 
     // Check password
-    if (this.comparePassword(password, user.password)) {
+    if (this.authProvider.comparePassword(password, user.password)) {
       // Make the jwt payload
       const jwtPayload = {
         email: email,
